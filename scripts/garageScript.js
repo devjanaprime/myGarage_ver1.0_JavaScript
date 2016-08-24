47806db25adb6e8b27a9ee55067ebfb62fe462f0
devjanaprime <dev@primeacademy.io>
2016-08-24 11:11:38 -0500

    appends array to ul on DOM

--- scripts/garageScript.js
+++ scripts/garageScript.js
@@ -20,5 +20,20 @@ var addCar = function( year, make, model, picURL, description ){
 
 var showCars = function(){
   console.log( 'in showCars');
-  console.log( myGarage );
+  // write each record of the array to an ul element on dom with id "carsOut"
+  // empty carsOut ul
+  document.getElementById( 'carsOut' ).innerHTML = '';
+  // for each car in the garage array, add a list item with it's text
+  for( var i = 0; i < myGarage.length; i++ ){
+    // create a new list item
+    var listItem = document.createElement( 'li' );
+    // assemble text for the current car
+    var carInfo = myGarage[ i ].year + ' ' + myGarage[ i ].make + ' ' + myGarage[ i ].model + ': ' + myGarage[ i ].description + ', picURL: ' + myGarage[ i ].picURL;
+    // create text node for text within list item with the car info
+    var textNode = document.createTextNode( carInfo );
+    // append text node to list item
+    listItem.appendChild( textNode );
+    // append list item to ul
+    document.getElementById( 'carsOut' ).appendChild( listItem );
+  } // end for
 }; // end showCars
